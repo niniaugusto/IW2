50d85ebfb1651188b217d9d29501b31bbaf0c692
niniaugusto <nlsn6132@gmail.com>
2024-03-30 19:53:24 -0300

    alteração

--- AULA_2 (Lampada)/JS/script.js
+++ AULA_2 (Lampada)/JS/script.js
@@ -1,30 +1,56 @@
 const ligar = document.getElementById('ligar')
 const desligar = document.getElementById('desligar')
-const lampada = document.getElementById('lamp')
+const lampada = document.getElementById('lampada')
+const bduplo = document.getElementById('bduplo')
+const texto = document.getElementById('texto')
+
 
 function estaQuebrada(){
-    return lampada.src.indexOf('quebrada')> -1
+    return lampada.src.indexOf('quebrada') > -1
+    texto.innerHTML ="LIGADA"
 }
 
-
-function lampDesligada(){
+function lampadaligar(){
     if(!estaQuebrada()){
-        lampada.src= "img/desligada.jpg"
+    lampada.src = "img/ligada.jpg"
     }
+    texto.innerHTML ="LIGADA"
 }
-function lampLigada(){
+
+function lampadadesligada(){
     if(!estaQuebrada()){
-        lampada.src= "img/ligada.jpg"
+    lampada.src = "img/desligada.jpg"
     }
-}
-function lampQuebrada(){
-    lampada.src = "img/quebrada.jpg"
+    texto.innerHTML ="DESLIGADA"
 }
 
-ligar.addEventListener('click',lampLigada)
-desligar.addEventListener('click',lampDesligada)
+function lampadaquebrada(){
+    lampada.src= "img/quebrada.jpg"
+    texto.innerHTML ="QUEBRADA"
+}
 
-lampada.addEventListener('mouseover', lampLigada)
-lampada.addEventListener('mouseleave',lampDesligada)
+function ligadesliga() {
+    if (bduplo.textContent=='ligar'){
+        lampadaligar();
+        bduplo.textContent='desligar';
+    } 
+    
+    else{
+        lampadadesligada();
+        bduplo.textContent='ligar';
+    }
+    texto.innerHTML ="LIGA/DESLIGA"
+ 
+}
 
-lampada.addEventListener('dblclick',lampQuebrada)
+
+
+ligar.addEventListener('click',lampadaligar)
+desligar.addEventListener('click',lampadadesligada)
+
+lampada.addEventListener('mouseover',lampadaligar)
+lampada.addEventListener('mouseleave',lampadadesligada)
+
+lampada.addEventListener('dblclick',lampadaquebrada)
+
+bduplo.addEventListener('click',ligadesliga)
