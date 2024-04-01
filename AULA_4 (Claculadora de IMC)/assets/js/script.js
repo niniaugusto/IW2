d475e34d5a3b905b1e6ae7b79116f6dbef8acab6
niniaugusto <nlsn6132@gmail.com>
2024-03-31 21:06:15 -0300

    alteração

--- AULA_4 (Claculadora de IMC)/assets/js/script.js
+++ AULA_4 (Claculadora de IMC)/assets/js/script.js
@@ -1,10 +1,10 @@
-const form = document.getElementById('form')
+const form = document.getElementById('form');
 
 form.addEventListener('submit', function(event) {
     event.preventDefault();
 
     const weight = document.getElementById('weight').value;
-    const height = document.getElementById('weight').value;
+    const height = document.getElementById('height').value;
 
     const bmi = (weight / (height * height)).toFixed(2);
 
@@ -16,7 +16,7 @@ form.addEventListener('submit', function(event) {
     document.getElementById('infos').classList.remove('hidden');
     
     if (bmi < 18.5) {
-        description = 'Cuidado! Você está abaixo do peso!'
+        description = 'Cuidado! Você está abaixo do peso!';
     } else if ( bmi >= 18.5 && bmi <= 25 ) {
         description = "Você está no peso ideal!";
         value.classList.remove('attention');
@@ -24,11 +24,11 @@ form.addEventListener('submit', function(event) {
     } else if (bmi > 25 && bmi <= 30) {
         description = "Cuidado! Você está com sobrepeso!";
     } else if (bmi > 30 && bmi <= 35) {
-        description = "Cuidado! Você etá com obesidade moderada!";
+        description = "Cuidado! Você está com obesidade moderada!";
     } else if (bmi > 35 && bmi <= 40) {
         description = "Cuidado! Você está com obesidade severa!";
     } else {
-        description = "Cuidado! Você esta com obesidade morbida!"
+        description = "Cuidado! Você está com obesidade mórbida!";
     }
 
     value.textContent = bmi.replace('.',',');
