28c2a48d6409506af971de1021aa5fbf25102040
niniaugusto <nlsn6132@gmail.com>
2024-09-30 08:05:55 -0300

    alteração

--- 3°Bimestre/react/etecflix-main/src/components/Footer/index.js
+++ 3°Bimestre/react/etecflix-main/src/components/Footer/index.js
@@ -3,7 +3,7 @@ import styles from './Footer.module.css';
 function Footer(){
     return(
         <footer className={styles.footer}>
-            <h2>Desenvolvido por Paulo Augusto &reg; 2024</h2>
+            <h2>Desenvolvido por Nícolas Augusto &reg; 2024</h2>
         </footer>
     );
 }
